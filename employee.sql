aed0a0419adf6945e84ca6f56e0f1f41027b0660
gsuStudent <35585938+gsuStudent@users.noreply.github.com>
2018-10-24 13:11:52 -0400

    UPDATED FILE USE THIS

--- employee.sql
+++ employee.sql
@@ -1,23 +1,17 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.15.5
--- http://www.phpmyadmin.net
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Jan 23, 2017 at 06:43 PM
--- Server version: 5.5.49-log
--- PHP Version: 7.0.9
+-- Host: localhost:8889
+-- Generation Time: Oct 24, 2018 at 05:09 PM
+-- Server version: 5.7.23
+-- PHP Version: 7.2.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
--- Database: `school`
+-- Database: `test`
 --
 
 -- --------------------------------------------------------
@@ -26,8 +20,9 @@ SET time_zone = "+00:00";
 -- Table structure for table `employee`
 --
 
-CREATE TABLE IF NOT EXISTS `employee` (
-  `Id` int(11) NOT NULL,
+CREATE TABLE `employee` (
+  `id` int(11) NOT NULL,
+  `EmployeID` text NOT NULL,
   `first_name` text NOT NULL,
   `last_name` text NOT NULL,
   `phone` text NOT NULL,
@@ -39,16 +34,28 @@ CREATE TABLE IF NOT EXISTS `employee` (
 -- Dumping data for table `employee`
 --
 
-INSERT INTO `employee` (`Id`, `first_name`, `last_name`, `phone`, `email`, `skills`) VALUES
-(23425, 'Michelle', 'Kir', '678-322-1233', 'Kir121@gmail.com', 'Java'),
-(61123, 'Don', 'Jo', '404-321-3421', 'joDon@hotmail.com', 'C++,C#,C'),
-(42234, 'Boer', 'King', '201-322-1101', 'KingPo12@yahoo.com', 'HTML,CSS,Javascript'),
-(21324, 'Dirr', 'Ig', '601-332-4564', 'DirIg@aol.com', 'Swift,Java,MySql'),
-(34334, 'Bob', 'Brown', '911-119-9191', 'Brown121B@gmail.com', 'Pascal,Lua,Python'),
-(87874, 'James', 'Kron', '322-121-4121', 'JamesK@yahoo.com', 'Perl,Ruby,Php'),
-(51223, 'Rea', 'Day', '678-122-3221', 'rea232@gmail.com', 'Fortran,Cobol');
+INSERT INTO `employee` (`id`, `EmployeID`, `first_name`, `last_name`, `phone`, `email`, `skills`) VALUES
+(1, '23121', 'Bob', 'Ross', '678-902-1223', 'bobo@gmail.com', 'python'),
+(13, '124189', 'Manu', 'Slol', '678-901-8811', 'slol@yahoo.com', 'Java, C'),
+(14, '12416', 'Don', 'Jo', '404-101-2211', 'jol@hotmail.com', 'C++'),
+(15, '3234', 'John', 'Dike', '404-112-3021', 'dike@yahoo.com', 'Perl');
 
--- --------------------------------------------------------
+--
+-- Indexes for dumped tables
+--
 
-  ALTER TABLE `employee`
-  ADD PRIMARY KEY (`Id`);
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
